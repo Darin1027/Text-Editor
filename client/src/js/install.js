7aca045492c8958187a30c8943f35ce22a6e5e8c
DareEnd <114109917+Darin1027@users.noreply.github.com>
2023-01-10 11:06:18 -0700

    added code for eventlisteners in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -12,8 +12,23 @@ window.addEventListener("beforeinstallprompt", (event) => {
   butInstall.style.display = "block";
 });
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => {});
+butInstall.addEventListener("click", async () => {
+  // Show the prompt
+  window.deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  const choice = await window.deferredPrompt.userChoice;
+  if (choice.outcome === "accepted") {
+    console.log("User accepted the A2HS prompt");
+  } else {
+    console.log("User dismissed the A2HS prompt");
+  }
+  // Hide the "Add to Home Screen" button
+  butInstall.style.display = "none";
+  // Clear the deferred prompt variable
+  window.deferredPrompt = null;
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener("appinstalled", (event) => {});
+window.addEventListener("appinstalled", (event) => {
+  console.log("PWA Installed", event);
+  alert("PWA Installed Successfully!");
+});
