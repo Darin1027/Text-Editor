96931c21ae3baa63d1391257956db8be2e86ec89
DareEnd <114109917+Darin1027@users.noreply.github.com>
2023-01-10 09:55:32 -0700

    added code for logic for installing the PWA in install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,19 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (event) => {
+  // Prevent the default prompt
+  event.preventDefault();
+
+  // Store the event to use it later
+  window.deferredPrompt = event;
+
+  // Show the "Add to Home Screen" button
+  butInstall.style.display = "block";
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", async () => {});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (event) => {});
